34c34fc550d544488b440d9e8f61f28c46ccea45
Axel Forsman <axelsfor@gmail.com>
2022-07-11 08:27:43 +0200

    Some WIP things

--- default.nix
+++ default.nix
@@ -1,12 +1,14 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  inherit (builtins) head elemAt listToAttrs getAttr hasAttr;
-  inherit (pkgs) lib;
-  inherit (pkgs.beamPackages.callPackage ./lib.nix {} {}) readErl;
+  inherit (builtins) head elemAt listToAttrs getAttr hasAttr groupBy toString zipAttrsWith;
+  inherit (pkgs) lib beamPackages;
+  inherit (beamPackages.callPackage ./lib.nix {} {}) readErl;
+
+  buildRebar3' = beamPackages.callPackage ./build-rebar3.nix {};
 
   supportedConfigVsns = [ "1.2.0" ];
-in {
+in rec {
   buildRebar3 = {
     path, pname, version
   }: let
@@ -20,35 +22,52 @@ in {
         (x: head x == "pkg_hash_ext")
         (throw "pkg_hash_ext not found")
         attrs) 1));
-    deps = map
+    deps = { "0" = []; } // zipAttrsWith (_lvl: deps: deps) (map
       (x: let
         name = head x;
         src = elemAt x 1;
+        lvl = elemAt x 2;
         tbl = {
-          pkg = pkgs.beamPackages.buildHex {
-            inherit name;
-            hexPkg = elemAt src 1;
+          pkg = let
+            pkg = elemAt src 1;
             version = elemAt src 2;
-            sha256 = getAttr name hashes;
+          in buildRebar3 {
+            pname = pkg;
+            inherit version;
+            path = beamPackages.fetchHex {
+              inherit pkg version;
+              sha256 = getAttr name hashes;
+            };
+            # beamDeps = if hasAttr (toString (lvl + 1)) deps
+            #            then getAttr (toString (lvl + 1)) deps
+            #            else [];
+          };
+          git = buildRebar3' {
+            inherit name;
+            version = "git";
+            src = fetchGit {
+              url = elemAt src 1;
+              rev = elemAt (elemAt src 2) 1;
+            };
           };
         };
         type = head src;
       in if builtins.hasAttr type tbl
-         then builtins.getAttr type tbl
+         then { "${toString lvl}" = builtins.getAttr type tbl; }
          else throw "Unsupported dependency type ${type} for ${name}")
-      locks;
+      locks);
 
     rel = lib.trivial.warnIfNot
       (builtins.elem vsn supportedConfigVsns)
       "Unsupported lock file. Proceeding anyway..."
-      (pkgs.beamPackages.rebar3Relx {
+      (beamPackages.rebar3Relx {
         # name = pname;
         inherit pname version;
         src = path;
 
         # REBAR_IGNORE_DEPS = true;
 
-        beamDeps = deps;
+        beamDeps = deps."0";
 
         releaseType = "escript";
       });
