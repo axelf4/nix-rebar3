2aeb21f829a5b8160731eb9f7ef63a49d4089198
Axel Forsman <axelsfor@gmail.com>
2022-07-26 18:59:11 +0200

    Allow specifying additional checkout dependencies
    This can be necessary since rebar3 lock files do not include plugin
    dependencies nor dependencies not listed in the default profile.

--- default.nix
+++ default.nix
@@ -11,38 +11,42 @@ in {
       pname,
       version,
       releaseType,
-      profile ? "default"
-  }@userAttrs: let
-    terms = readErl (root + "/rebar.lock");
-    vsn = head (head terms);
-    locks = elemAt (head terms) 1;
-    attrs = elemAt terms 1;
-    hashes = listToAttrs (map
-      (x: { name = head x; value = elemAt x 1; })
-      (elemAt (lib.findFirst
-        (x: head x == "pkg_hash_ext")
-        (throw "pkg_hash_ext not found")
-        attrs) 1));
-    deps = listToAttrs (map (x: let
-      name = head x;
-      src = elemAt x 1;
-      tbl = {
-        pkg = beamPackages.fetchHex {
-          pkg = elemAt src 1;
-          version = elemAt src 2;
-          sha256 = getAttr name hashes;
+      profile ? "default",
+      checkouts ? {}
+  }@attrs: let
+    deps = let
+      terms = readErl (root + "/rebar.lock");
+      vsn = head (head terms);
+      locks = elemAt (head terms) 1;
+      attrs = elemAt terms 1;
+      hashes = listToAttrs (map
+        (x: { name = head x; value = elemAt x 1; })
+        (elemAt (lib.findFirst
+          (x: head x == "pkg_hash_ext")
+          (throw "pkg_hash_ext not found")
+          attrs) 1));
+    in lib.trivial.warnIfNot (elem vsn supportedConfigVsns)
+      "Unsupported lock file. Proceeding anyway..."
+      (listToAttrs (map (x: let
+        name = head x;
+        src = elemAt x 1;
+        tbl = {
+          pkg = beamPackages.fetchHex {
+            pkg = elemAt src 1;
+            version = elemAt src 2;
+            sha256 = getAttr name hashes;
+          };
+          git = fetchGit {
+            url = elemAt src 1;
+            rev = elemAt (elemAt src 2) 1;
+          };
+          path = root + "/${elemAt src 1}";
         };
-        git = fetchGit {
-          url = elemAt src 1;
-          rev = elemAt (elemAt src 2) 1;
-        };
-        path = root + "/${elemAt src 1}";
-      };
-      type = head src;
-    in if hasAttr type tbl
-       then { inherit name; value = getAttr type tbl; }
-       else throw "Unsupported dependency type ${type} for ${name}")
-      locks);
+        type = head src;
+      in if hasAttr type tbl
+         then { inherit name; value = getAttr type tbl; }
+         else throw "Unsupported dependency type ${type} for ${name}")
+        locks) // checkouts);
 
     depsDrv = stdenv.mkDerivation {
       pname = "${pname}-deps";
@@ -67,13 +71,11 @@ in {
       dontInstall = true;
     };
 
-    rel = lib.trivial.warnIfNot (elem vsn supportedConfigVsns)
-      "Unsupported lock file. Proceeding anyway..."
-      (stdenv.mkDerivation (userAttrs // {
+    rel = stdenv.mkDerivation (attrs // {
         inherit pname version;
         src = root;
 
-        buildInputs = userAttrs.buildInputs or [] ++ [ erlang rebar3 ];
+        buildInputs = attrs.buildInputs or [] ++ [ erlang rebar3 ];
 
         REBAR_OFFLINE = true;
 
@@ -103,7 +105,7 @@ in {
 
         meta = {
           inherit (erlang.meta) platforms;
-        } // userAttrs.meta or {};
-      }));
+        } // attrs.meta or {};
+      });
   in rel;
 }
