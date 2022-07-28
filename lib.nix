47affb7001d2f93dc399267ac622d7a0e5c35f23
Axel Forsman <axelsfor@gmail.com>
2022-07-28 21:25:23 +0200

    Simplify parser

--- lib.nix
+++ lib.nix
@@ -8,36 +8,35 @@
 }:
 
 let
-  inherit (builtins) substring fromJSON readFile;
+  inherit (builtins) head match stringLength substring fromJSON readFile;
 
-  skipWs = s: builtins.elemAt (builtins.match "([[:space:]]|%[^\n]*\n?)*(.*)" s) 1;
-  sepBy = { beg ? "", sep, end ? null }: c: s: let
-    c' = x: s: c x (if end == null then "" else substring (builtins.stringLength end) (-1) s);
-    isEnd = if end == null then s: s == "" else lib.hasPrefix end;
-    f = acc: term (x: s:
+  skipWs = s: builtins.elemAt (match "([[:space:]]|%[^\n]*\n?)*(.*)" s) 1;
+  sepBy = { beg ? "", sep, end ? "" }: s: c: let
+    c' = s: c (substring (stringLength end) (-1) s);
+    f = s: acc: term s (s: x:
       let s' = skipWs s;
           acc' = acc ++ [x];
       in if lib.hasPrefix sep s'
-         then let s'' = skipWs (substring (builtins.stringLength sep) (-1) s');
-              in (if s'' == "" then c' else f) acc' s''
-         else c' acc' s');
-    s' = skipWs (substring (builtins.stringLength beg) (-1) s);
-  in if isEnd s' then c' [] s' else f [] s';
+         then let s'' = skipWs (substring (stringLength sep) (-1) s');
+              in (if s'' == "" then c' else f) s'' acc'
+         else c' s' acc');
+    s' = skipWs (substring (stringLength beg) (-1) s);
+  in (if end != "" -> !lib.hasPrefix end s' then f else c') s' [];
 
   list = sepBy { beg = "["; sep = ","; end = "]"; };
   tuple = sepBy { beg = "{"; sep = ","; end = "}"; };
 
-  parseString = isBin: c: s: let
-    x = builtins.head (builtins.match ''${if isBin then "<<" else ""}("(\\"|[^"])*").*'' s);
-  in c (builtins.fromJSON x)
-    (substring (builtins.stringLength x + (if isBin then 4 else 0)) (-1) s);
+  parseString = isBin: s: c: let
+    x = head (match ''${if isBin then "<<" else ""}("(\\"|[^"])*").*'' s);
+  in c (substring (stringLength x + (if isBin then 4 else 0)) (-1) s)
+    (fromJSON x);
 
-  number = c: s: let
-    x = builtins.head (builtins.match ''([0-9]+).*'' s);
-  in c (builtins.fromJSON x) (substring (builtins.stringLength x) (-1) s);
-  atom = c: s: let
-    x = builtins.head (builtins.match ''([a-z][A-Za-z0-9_@]*).*'' s);
-  in c x (substring (builtins.stringLength x) (-1) s);
+  number = s: c: let
+    x = head (match ''([0-9]+).*'' s);
+  in c (substring (stringLength x) (-1) s) (fromJSON x);
+  atom = s: c: let
+    x = head (match ''([a-z][A-Za-z0-9_@]*).*'' s);
+  in c (substring (stringLength x) (-1) s) x;
 
   term = let
     tbl = {
@@ -46,12 +45,12 @@ let
       "0" = number; "1" = number; "2" = number; "3" = number; "4" = number;
       "5" = number; "6" = number; "7" = number; "8" = number; "9" = number;
     };
-  in c: s: let
+  in s: let
     ch = substring 0 1 s;
-  in (if builtins.hasAttr ch tbl then builtins.getAttr ch tbl else atom) c s;
+  in (if builtins.hasAttr ch tbl then builtins.getAttr ch tbl else atom) s;
 
   consult = sepBy { sep = "."; };
-  fromErl = s: consult (x: _s: x) (skipWs s);
+  fromErl = s: consult (skipWs s) (_s: x: x);
 
   jsone = buildHex {
     name = "jsone";
